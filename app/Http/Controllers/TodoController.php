1b6253b3c562adcbc264b2e595e5a04faf9f726d
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-04-15 18:40:15 +0700

    update, delete data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -17,7 +17,11 @@ class TodoController extends Controller
         ->orderBy('is_complete', 'asc')
         ->orderBy('created_at', 'desc')
         ->get();
-        return view('todo.index', compact('todos'));
+        // dd($todos);
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+        ->where('is_complete', true)
+        ->count();
+        return view('todo.index', compact('todos', 'todosCompleted'));
     }
 
     /**
@@ -58,6 +62,7 @@ class TodoController extends Controller
         return redirect()->route('todo.index')->with('success', 'Todo Created Succcessfully!');
     }
 
+
     /**
      * Display the specified resource.
      */
@@ -71,15 +76,68 @@ class TodoController extends Controller
      */
     public function edit(Todo $todo)
     {
-        return view('todo.edit');
+        if(auth()->user()->id == $todo->user_id){
+            //dd($todo);
+
+            return view('todo.edit', compact('todo'));
+        }else{
+            // abort(403);
+            //abort(403, 'Not authorized');
+
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to edit this todo!');
+        }
     }
 
+    public function complete(Todo $todo)
+    {
+        if(auth()-> user()->id == $todo->user_id){
+            $todo->update([
+                'is_complete' => true,
+            ]);
+
+            return redirect()->route('todo.index')->with('success', 'Todo completed successfully!');
+
+        }else{
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to complete this todo!');
+
+        }
+    }
+
+    public function uncomplete(Todo $todo)
+    {
+        if(auth()-> user()->id == $todo->user_id){
+            $todo->update([
+                'is_complete' => false,
+            ]);
+
+            return redirect()->route('todo.index')->with('success', 'Todo uncompleted successfully!');
+
+        }else{
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to uncomplete this todo!');
+
+        }
+    }
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Todo $todo)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255'
+        ]);
+
+
+
+        //Practical
+        //$todo->title = $request->title;
+        //$todo->save();
+
+        //Eloquent Way - Readable
+
+        $todo->update([
+            'title' => ucfirst($request->title),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo Updated Successfully!');
     }
 
     /**
@@ -87,6 +145,24 @@ class TodoController extends Controller
      */
     public function destroy(Todo $todo)
     {
-        //
+        if(auth()->user()->id == $todo->user_id){
+
+            $todo->delete();
+            return redirect()->route('todo.index')->with('success', 'Todo deleted successsfully!');
+
+        }else{
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to delete this todo!');
+        }
+    }
+
+    public function destroyCompleted(){
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+        ->where('is_complete', true)
+        ->get();
+        foreach($todosCompleted as $todo){
+            $todo->delete();
+        }
+
+        return redirect()->route('todo.index')->with('success', 'All Completed todos deleted successfully!');
     }
 }
---
+++ tugas6.md
@@ -0,0 +1,33 @@
+## Tugas 6
+
+## 1. Todo Completed
+
+![Alt text](screenshoot/tugas6/TodoCompleted.png)
+
+## 2. Todo Uncompleted
+
+![Alt text](screenshoot/tugas6/TodoUncompleted.png)
+
+## 3. Todo Edited
+
+![Alt text](screenshoot/tugas6/TodoEdited.png)
+
+## 4. Todo Deleted
+
+![Alt text](screenshoot/tugas6/TodoDeleted.png)
+
+## 5. Todo Deleted All Completed
+
+![Alt text](screenshoot/tugas6/TodoDeleteAllCompleted.png)
+
+## 6. Todo Make Admin
+
+![Alt text](screenshoot/tugas6/UserMakeAdmin.png)
+
+## 7. Todo Remove Admin
+
+![Alt text](screenshoot/tugas6/UserRemoveAdmin.png)
+
+## 8. Todo Deleted
+
+![Alt text](screenshoot/tugas6/UserDeleted.png)
