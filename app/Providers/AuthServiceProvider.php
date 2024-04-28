2a219a6785e2ad5d8b88cc88fe23215187ff612a
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-04-28 20:02:59 +0700

    middleware, gate admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -16,11 +20,11 @@ class AuthServiceProvider extends ServiceProvider
         //
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
+
     public function boot(): void
     {
-        //
+        Gate::define('admin', function (User $user){
+            return $user->is_admin == true;
+        });
     }
 }
---
+++ tugas7.md
@@ -0,0 +1,9 @@
+## Tugas 7
+
+## 1. Middleware & Gate Admin Active
+
+![Alt text](screenshoot/tugas7/Middleware&GateAdminActive.png)
+
+## 2. Middleware & Date Admin Not Active
+
+![Alt text](screenshoot/tugas7/Middleware&GateAdminNotActive.png)
